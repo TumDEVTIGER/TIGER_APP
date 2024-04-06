7008253a0571ed439e3242b3516552a9dd9eafbc
paiboonmt <166192944+TumDEVTIGER@users.noreply.github.com>
2024-04-06 10:41:02 +0700

    update

---
+++ BackEND/Database/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+    $servername = "172.16.0.254";
+    $username = "admin";
+    $password = "9itonly";
+    $DB = "member";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$DB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+} 
